7bfcbc19d23c6c4c24abb42c35e58268b83ed437
Catalin Moldovan <139596+heretique@users.noreply.github.com>
2013-09-06 17:06:47 +0300

    * Continued work on key handling

--- horde3d/binaries/Content/lua/chicago.lua
+++ horde3d/binaries/Content/lua/chicago.lua
@@ -4,7 +4,8 @@ require("h3d")
 require("h3du")
 
 local app = {
-
+	_x = 15, _y = 3, _z = 20, _rx = -10, _ry = 60, _velocity = 10.0, _curFPS = 30,
+	_keys = {}
 }
 app._contentDir = "../Content"
 
@@ -23,6 +24,7 @@ end
 
 function onCreate()
 	print("onCreate")
+
 	local res = h3d.Init(H3DRenderers.OpenGL_ES_2_0)
 	if  res == false then
 		h3du.DumpMessages()
@@ -110,8 +112,9 @@ end
 
 function onRender()
 	--print("onRender")
+	keyHandler()
 	-- Render scene
-	h3d.SetNodeTransform( app._cam, 15, 3, 20, -10 ,30, 0, 1, 1, 1 )
+	h3d.SetNodeTransform( app._cam, app._x, app._y, app._z, app._rx ,app._ry, 0, 1, 1, 1 )
 	luacubeRot = luacubeRot + 2
     h3d.SetNodeTransform( luacube, 0, 2.5, 0, luacubeRot, luacubeRot, luacubeRot, 2, 2, 2 )
 	h3du.ShowFrameStats( app._fontMatRes, app._panelMatRes, 1 )
@@ -128,17 +131,53 @@ end
 
 function keyPressEvent(key)
 	print("keyPressEvent: " .. key)
+	app._keys[key] = true
+
 end
 
 function keyReleaseEvent(key)
 	print("keyReleaseEvent: " .. key)
+	app._keys[key] = nil
 end
 
 function mousePressEvent(buttons, posX, poxY)
 end
 
-function mouseMoveEvent(posX, posY)
+function mouseMoveEvent(dX, dY)
+	app._ry = app._ry - (dX / 100 * 30)
+	
+	-- Loop up/down but only in a limited range
+	app._rx = app._rx + (dY / 100 * 30)
+	if( app._rx > 90 )  then app._rx = 90 end
+	if( app._rx < -90 ) then app._rx = -90 end
 end
 
 function mouseReleaseEvent(buttons, posX, poxY)
+end
+
+function keyHandler()
+
+		local curVel = app._velocity / app._curFPS
+
+		if app._keys[Key.Key_Shift] == true then curVel = curVel * 5	end -- LShift
+		
+		if app._keys[Key.Key_W] == true then
+			-- Move forward
+			app._x = app._x - math.sin( degToRad( app._ry ) ) * math.cos( -degToRad( app._rx ) ) * curVel;
+			app._y = app._y - math.sin( -degToRad( app._rx ) ) * curVel;
+			app._z = app._z - math.cos( degToRad( app._ry ) ) * math.cos( -degToRad( app._rx ) ) * curVel;
+		elseif app._keys[Key.Key_S] == true then
+			-- Move backward
+			app._x = app._x + math.sin( degToRad( app._ry ) ) * math.cos( -degToRad( app._rx ) ) * curVel;
+			app._y = app._y + math.sin( -degToRad( app._rx ) ) * curVel;
+			app._z = app._z + math.cos( degToRad( app._ry ) ) * math.cos( -degToRad( app._rx ) ) * curVel;
+		elseif app._keys[Key.Key_A] then
+			-- Strafe left
+			app._x = app._x + math.sin( degToRad( app._ry - 90) ) * curVel;
+			app._z = app._z + math.cos( degToRad( app._ry - 90 ) ) * curVel;
+		elseif app._keys[Key.Key_D] then
+			-- Strafe right
+			app._x = app._x + math.sin( degToRad( app._ry + 90 ) ) * curVel;
+			app._z = app._z + math.cos( degToRad( app._ry + 90 ) ) * curVel;
+		end
 end
